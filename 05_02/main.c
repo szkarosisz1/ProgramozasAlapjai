b101eda58aa08a71ed92dc3e29227d14c769890f
szkarosisz1 <90700865+szkarosisz1@users.noreply.github.com>
2021-10-26 19:10:36 +0200

    5.Gyak

---
+++ 05_02/main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    //2. feladat a) része
+
+    int i, db=0;
+    /* Tömb létrehozása inicializációs listával */
+
+    double tomb[ ] = {289.5, 292.6, 290.2, 295.5, 300.4, 300.3, 302.5, 303.3, 303.5, 299.9};
+    int meret = sizeof(tomb)/sizeof(double);   //tömb méretének kiszámítása
+    double limit=300.0;
+
+    printf("HUF/EUR arfolyamok:\n");
+
+    for (i = 0; i < meret; i++)
+    {
+        printf("%d. ertek: \n", i+1, tomb[i]);
+
+    /* Feltételt kielégítõ elemek megszámlálása */
+        if (tomb[i] < limit)
+            db++;
+    }
+    printf("Az arfolyam erteke %d-szer volt %.2f alatt.", db, limit);
+
+    printf("");
+
+    return 0;
+}
