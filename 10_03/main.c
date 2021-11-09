b31d4861edd049dd2c00116ff0b7b8bdd5e56710
szkarosisz1 <90700865+szkarosisz1@users.noreply.github.com>
2021-11-09 16:24:39 +0100

    Update main.c

--- 10_03/main.c
+++ 10_03/main.c
@@ -16,39 +16,49 @@ int main()
 {
     int t[N];
     tombFeltolt(t);
+  
     struct tomb paros = tombSzetvalogat(t, 0);
     struct tomb paratlan = tombSzetvalogat(t, 1);
+  
     printf("\nParos elemek: ");
     tombKiir(paros);
+  
     printf("\nParatlan elemek: ");
     tombKiir(paratlan);
+  
     return 0;
 }
 
 void tombFeltolt(int *tomb){
     int i;
     srand(time(0));
+  
     for(i = 0; i < N; i++)
         tomb[i] = rand()%100+1;
+  
     return ;
 }
 
 void tombKiir(struct tomb t){
     int i;
+  
     for(i = 0; i < t.db; i++)
         printf("%d, ",t.eredmeny[i]);
+  
     return ;
 }
 
 struct tomb tombSzetvalogat(int *tomb, int paritas){
     struct tomb e;
     int i, j  = 0;
+  
     for(i = 0; i < N; i++){
-            if(tomb[i]%2==paritas){
+            if(tomb[i] % 2 == paritas){
                 e.eredmeny[j] = tomb[i];
                 j++;
             }
     }e.db = j;
+  
     return e;
 }
 
